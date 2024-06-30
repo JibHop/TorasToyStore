df408a98b44c9028fdee502bc92a4b938513963e
JibHop <enda29003@gmail.com>
2024-06-30 17:29:05 +0100

    Mod Setup (sorry it took so long :shy:)

--- src/main/java/com/equus/toras_toystore/TorasToyStore.java
+++ src/main/java/com/equus/toras_toystore/TorasToyStore.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.equus.toras_toystore;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Block;
@@ -18,13 +18,13 @@ import org.slf4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("toras_ts")
+public class TorasToyStore
 {
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public ExampleMod()
+    public TorasToyStore()
     {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
@@ -47,7 +47,7 @@ public class ExampleMod
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
         // Some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+        InterModComms.sendTo("toras_ts", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event)
